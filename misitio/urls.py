3e2279d6eafbf767ace75e9b4179a34e1db8d270
jony <apolion_7_j@hotmail.com>
2014-06-27 18:05:03 +0200

    examen

---
+++ misitio/urls.py
@@ -0,0 +1,16 @@
+#CONDING: UTF-8
+from django.conf.urls import patterns, include, url
+from polls import views
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'misitio.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+
+    url(r'^polls/', include('polls.urls',namespace="polls")),
+    url(r'^admin/', include(admin.site.urls)),
+    
+)
---
+++ polls/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll, Choice
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+# Create your views here.
+
+class IndexView (generic.ListView):
+    #latest_poll_list = Poll.objects.order_by('pub_date')[:5]
+    template_name = 'polls/index.html'
+    Context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """ devuelve las ultimas 5 encuestas publicadas."""
+        return Poll.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([p.question for p in latest_poll_list])
+
+    #return render(request,'polls/index.html',context)
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+    #try:
+     #   poll = get_object_or_404(Poll,pk=poll_id)
+    #except Poll.DoesNotExist:
+     #   raise Http404
+    #return render(request,'polls/detail.html',{'poll':poll})
+
+class ResultsView (generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+   # poll = get_object_or_404(Poll, pk=poll_id)
+   # return render(request, 'polls/results.html',{'poll':poll})
+
+def vote (request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, 'polls/detail.html',{
+            'poll':p,
+            'error_message':"usted no ha seleccionado una opcion.",
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(p.id)))
+
